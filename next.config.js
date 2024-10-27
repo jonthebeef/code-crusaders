e01ecec1b265a903f47fb4ce72ff5c8b1eb29f5b
Jon Grant <jonthebeef@gmail.com>
2024-10-27 15:08:01 +0000

    trying to get this to move to gh pages

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
   images: {
     domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
   },
+  basePath: '/code-crusaders',
+  assetPrefix: '/code-crusaders/',
 }
 
 module.exports = nextConfig
