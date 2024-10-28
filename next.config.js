a29c43069820eccb51f69126e42bff1db638cc85
Jon Grant <jonthebeef@gmail.com>
2024-10-28 20:25:06 +0000

    update config and stem

--- next.config.js
+++ next.config.js
@@ -3,11 +3,26 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    path: '/code-crusaders',
   },
   basePath: '/code-crusaders',
   assetPrefix: '/code-crusaders/',
   trailingSlash: true,
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next',
+            name: 'static/media/[name].[hash].[ext]',
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
