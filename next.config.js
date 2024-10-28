92b02bd6ded56f395073bf9de43a24986c2dc297
Jon Grant <jonthebeef@gmail.com>
2024-10-28 18:35:51 +0000

    changed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  reactStrictMode: true,
-  // other configurations...
   images: {
-    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
     unoptimized: true,
   },
   basePath: '/code-crusaders',
-  assetPrefix: '/code-crusaders/',
 }
 
 module.exports = nextConfig
