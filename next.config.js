02442a3981cec3fd6f88f9c59c0c826c9a79fffd
Jon Grant <jonthebeef@gmail.com>
2024-10-28 18:28:45 +0000

    Update Next.js configuration for static export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   // other configurations...
   images: {
     domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+    unoptimized: true,
   },
   basePath: '/code-crusaders',
   assetPrefix: '/code-crusaders/',
