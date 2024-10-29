d96a378211f326c8647052791a0107dac74f9758
Jon Grant <jonthebeef@gmail.com>
2024-10-29 09:42:53 +0000

    arrrrrrgggggh

--- app/components/StemEducation.tsx
+++ app/components/StemEducation.tsx
@@ -4,7 +4,7 @@ export default function StemEducation() {
   return (
     <div className="relative w-full md:w-[90%] h-[400px]">
       <img
-        src="https://jonthebeef.xyz/code-crusaders/images/children-coding.jpg"
+        src="https://jonthebeef.xyz/code-crusaders/images/children-coding.jpg?v=1"
         alt="Children excited about coding"
         className="rounded-lg shadow-lg object-cover"
       />
